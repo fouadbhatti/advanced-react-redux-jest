ef222b5f72f6f91808f8a7ee82adda98df3a11e4
Ahmed Fouad <fouad.bhatti@gmail.com>
2019-01-01 02:29:32 +0500

    Updated react / redux / enzyme to latest version

--- src/components/AddFriend.test.js
+++ src/components/AddFriend.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 import { AddFriend, AddFriendButton } from '../components';
 import renderer from 'react-test-renderer';
 
